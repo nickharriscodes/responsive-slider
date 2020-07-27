1fae1e74b03ff7870db4c4e3df02ccec9a5bf29b
Nick Harris <you@example.com>
2020-07-27 13:08:29 -0500

    Add container and animate

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function plusSlides(n) {
   
   setTimeout(function(){
     showSlides(slideIndex += n, n);
-  }, 500);
+  }, 400);
 }
 
 // Thumbnail image controls
@@ -15,20 +15,23 @@ function currentSlide(n) {
   handleSlideout("dot");
   setTimeout(function() {
     showSlides((slideIndex = n), "dot");
-  }, 500)
+  }, 400)
 }
 
 function handleSlideout(n) {
   var slides = document.getElementsByClassName("mySlides");
   var text = document.getElementsByClassName("text");
+  var containers = document.getElementsByClassName("slide-link");
   if (n === -1) {
     for (let i = 0; i < slides.length; i++) {
       if (slides[i].style.display == "block") {
         slides[i].style.animationName = "slide-out-left";
         text[i].style.animationName = "slide-out-left";
+        containers[i].style.animationName = "slide-out-left"
       } else {
         slides[i].style.animationName = "";
         text[i].style.animationName = "";
+        containers[i].style.animationName = "";
       }
     }
   }
@@ -37,9 +40,11 @@ function handleSlideout(n) {
       if (slides[i].style.display == "block") {
         slides[i].style.animationName = "slide-out-right";
         text[i].style.animationName = "slide-out-right";
+        containers[i].style.animationName = "slide-out-right"
       } else {
         slides[i].style.animationName = "";
         text[i].style.animationName = "";
+        containers[i].style.animationName = "";
       }
     }
   }
@@ -48,9 +53,11 @@ function handleSlideout(n) {
       if (slides[i].style.display == "block") {
         slides[i].style.animationName = "fade-out";
         text[i].style.animationName = "fade-out";
+        containers[i].style.animationName = "fade-out"
       } else {
         slides[i].style.animationName = "";
         text[i].style.animationName = "";
+        containers[i].style.animationName = ""
       }
     }
   }
@@ -61,6 +68,7 @@ function showSlides(n, x) {
   var slides = document.getElementsByClassName("mySlides");
   var text = document.getElementsByClassName("text");
   var dots = document.getElementsByClassName("dot");
+  var containers = document.getElementsByClassName("slide-link");
 
   if (n > slides.length) {
     slideIndex = 1
@@ -72,17 +80,21 @@ function showSlides(n, x) {
   if (x > 0) {
     slides[slideIndex-1].style.animationName = "slide-in-left";
     text[slideIndex-1].style.animationName = "slide-in-left";
+    containers[slideIndex-1].style.animationName = "slide-in-left";
   } else if (x < 0){
     slides[slideIndex-1].style.animationName = "slide-in-right";
     text[slideIndex-1].style.animationName = "slide-in-right";
+    containers[slideIndex-1].style.animationName = "slide-in-right";
   } else {
     slides[slideIndex-1].style.animationName = "fade-in";
     text[slideIndex-1].style.animationName = "fade-in";
+    containers[slideIndex-1].style.animationName = "fade-in";
   }
 
   for (i = 0; i < slides.length; i++) { 
     slides[i].style.display = "none";
     text[i].style.display = "none";
+    containers[i].style.display = "none"
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
@@ -91,4 +103,5 @@ function showSlides(n, x) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   text[slideIndex-1].style.display = "block";
+  containers[slideIndex-1].style.display = "flex";
 }
